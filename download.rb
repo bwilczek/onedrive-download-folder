90bebf238fc66c853272286770c307871f51b638
Bartek Wilczek <bwilczek@gmail.com>
2017-06-13 19:45:57 +0200

    Added support for download only files in root directory

--- download.rb
+++ download.rb
@@ -8,6 +8,7 @@ if starting_url.empty?
 end
 
 puts "URL: #{starting_url}"
+mode = ARGV[1]
 
 # set as container volume, change for local test without container
 download_dir = '/download'
@@ -23,11 +24,33 @@ end
 
 session = Capybara::Session.new(:firefox_auto_download)
 session.visit(starting_url)
-sleep 5
-session.find(:xpath, '//span[text()="Download"]').click
-sleep 5
 
-loop do
-  sleep 1
-  break unless Dir.entries(download_dir).to_s.include? '.zip.part'
+if mode.nil?
+  # default mode, download zipped folder
+  sleep 5
+  session.find(:xpath, '//span[text()="Download"]').click
+  sleep 5
+
+  loop do
+    sleep 1
+    break unless Dir.entries(download_dir).to_s.include? '.zip.part'
+  end
+
+elsif mode == 'root_files'
+  sleep 5
+  # only download files in the root directory, no recursion
+  elements = session.all(:xpath, '//div[contains(@class, "is-file")]//div[@class="ItemTile-name"]')
+  elements.each do |el|
+    puts el.text
+    el.click
+    sleep 2
+  end
+  puts "Downloading #{elements.count} elements"
+
+  loop do
+    sleep 1
+    listing = Dir.entries(download_dir)
+    break unless ( listing.to_s.include? '.kml.part' || listing.count < elements.count+2 )
+  end
+
 end
