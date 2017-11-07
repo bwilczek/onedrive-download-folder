6629cdc95d5bb8e1901227d789773a76b85b7fcb
Bartek Wilczek <bwilczek@gmail.com>
2017-11-07 09:00:01 +0100

    Added sleep after 'Download' button click

--- download.rb
+++ download.rb
@@ -37,11 +37,12 @@ if mode.nil?
   end
 
 elsif mode == 'root_files'
-  sleep 20
+  sleep 10
   # only download files in the root directory, no recursion
   elements = session.all(:xpath, '//div[contains(@class, "is-file")]//span[@class="ItemCheck"]')
   elements.each do |el|
     # puts el.find(:xpath, '//div[@class="ItemTile-name"]').text
+    puts " > Hover+Click on a file..."
     el.hover
     sleep 2
     el.click
@@ -50,9 +51,10 @@ elsif mode == 'root_files'
   puts "Downloading #{elements.count} elements to #{download_dir}"
   session.find(:xpath, '//span[text()="Download"]').click
   puts " > 'Download' link clicked"
+  sleep 5
 
   loop do
-    puts " > in  progress"
+    puts " > in progress"
     sleep 1
     # listing = Dir.entries(download_dir)
     # break unless ( listing.to_s.include? '.kml.part' || listing.count < elements.count+2 )
